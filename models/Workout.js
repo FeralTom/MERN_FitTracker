3aae6746138218876acc9386c71e60cc425aaefb
FeralTom <adensabalsa18@gmail.com>
2026-07-19 21:46:28 -0400

    Add files via upload

---
+++ models/Workout.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const workoutSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    exercise: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    sets: {
+      type: Number,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    weight: {
+      type: Number,
+      default: 0,
+    },
+    duration: {
+      type: Number,
+      default: 0,
+    },
+    notes: {
+      type: String,
+      default: "",
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Workout", workoutSchema);
